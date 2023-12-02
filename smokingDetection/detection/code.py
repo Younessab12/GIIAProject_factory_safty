3cbb7f5e83ed1bbfb0fa3cf540db07d50d7ee55f
younessab12 <younesseaabibi828@gmail.com>
2023-12-02 23:10:24 +0100

    test

---
+++ smokingDetection/detection/code.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+import cv2
+import numpy as np
+import time
+
+
+class Detector:
+    def __init__(self,model_path,cam_id=0):
+        self.model = YOLO(model_path)
+        self.cam_id = cam_id
+    
+
+    def detect(self):
+        print("starting detection")
+        curr_time = time.time()
+        cap = cv2.VideoCapture(self.cam_id)
+## phone detection 
+        while True:
+            ret,frame = cap.read()
+            frame = cv2.flip(frame,1)
+            res = self.model(frame,stream=True,show=False)
+            for r in res:
+                boxes = r.boxes
+                if len(boxes) > 0:
+                    print("smoking detected",time.time()-curr_time )
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
