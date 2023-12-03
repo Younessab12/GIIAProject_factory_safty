20c498e5ce5047f4b37435d862d2b0d60f091a95
AmineF0 <aminefirdawsi@gmail.com>
2023-12-03 20:18:43 +0100

    corrections

--- profiling&calib/code.py
+++ profiling&calib/code.py
@@ -1,9 +1,29 @@
-import cv2
-import numpy as np
 import time
+import cv2
 import math
 import json
 import os
+from imutils import paths
+import face_recognition
+import pickle
+import numpy as np
+
+"""
+    !!!
+        To use calib:
+
+            person_name = "your_name"
+            calib = Calib(person_name)
+
+            ## loop over the frames of the input
+                calib.process(frame, holistic_res)
+            ## end loop
+
+            calib.export_json()
+            calib.train_model_face_recognition()
+
+"""
+
 
 """
   To calibrate we will record the following:
@@ -106,6 +126,53 @@ class Calib:
         print(self.messages[self.state])
 
     return self.get_state() == len(calibrations)
+
+  def train_model_face_recognition(self):
+    """
+        this fct will train the model and save the encodings in a pickle file
+        param:
+             path_to_imgs_folder: path to the folder containing the images
+
+    """
+    print("[INFO] start processing faces...")
+    imagePaths = list(paths.list_images(self.face_recognition_dataset_folder))
+
+    # initialize the list of known encodings and known names
+    knownEncodings = []
+    knownNames = []
+
+    # loop over the image paths
+    for (i, imagePath) in enumerate(imagePaths):
+        # extract the person name from the image path
+        print("[INFO] processing image {}/{}".format(i + 1,
+            len(imagePaths)))
+        name = imagePath.split(os.path.sep)[-2]
+
+        # load the input image and convert it from RGB (OpenCV ordering)
+        # to dlib ordering (RGB)
+        image = cv2.imread(imagePath)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # detect the (x, y)-coordinates of the bounding boxes
+        # corresponding to each face in the input image
+        boxes = face_recognition.face_locations(rgb, model="hog")
+
+        # compute the facial embedding for the face
+        encodings = face_recognition.face_encodings(rgb, boxes)
+
+        # loop over the encodings
+        for encoding in encodings:
+            # add each encoding + name to our set of known names and
+            # encodings
+            knownEncodings.append(encoding)
+            knownNames.append(name)
+
+    # dump the facial encodings + names to disk
+    print("[INFO] serializing encodings...")
+    data = {"encodings": knownEncodings, "names": knownNames}
+    f = open(f"{self.folder}/encodings.pickle", "wb")
+    f.write(pickle.dumps(data))
+    f.close()
 
   def get_results(self):
     return {
@@ -301,6 +368,8 @@ class Calib:
     if not os.path.exists(face_recognition_folder):
       os.makedirs(face_recognition_folder)
 
+    self.folder = folder
+    self.face_recognition_dataset_folder = f'{folder}/data'
     self.face_recognition_folder = face_recognition_folder
     self.record_file = f'{folder}/calibration_{self.owner}'
 
@@ -308,3 +377,4 @@ class Calib:
     data = self.get_results()
     with open(f'{self.record_file}.json', 'w') as outfile:
       json.dump(data, outfile, cls=NumpyEncoder)
+
