a9cc005f7a9a1b3158c293eebcc0fd2ca9b3ff22
AmineF0 <aminefirdawsi@gmail.com>
2023-12-15 03:57:52 +0100

    admin refined

---
+++ admin-dashboard/src/app/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { useEffect, useState } from 'react'
+
+type RowType = {
+  timestamp: string,
+  operatorId: string,
+  activityName: string,
+  gravity: number,
+  raspberryName: string,
+}
+
+export default function HomePage() {
+  const [rows, setRows] = useState<RowType[]>([]);
+  const [lastRefresh, setLastRefresh] = useState("");
+  // timestamp operatorId activity gravity
+  useEffect(() => {
+    setInterval(async () => {
+      const last = await fetch('/api/notifications');
+      const lastJson = await last.json();
+      setRows(lastJson.rows);
+      setLastRefresh(lastJson.lastRefresh);
+    }, 3000);
+  }, [])
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <h1 className="text-4xl font-bold">Admin Dashboard</h1>
+      {/* {typeof(lastRefresh) == String ? lastRefresh : lastRefresh.toISOString()} */}
+      {lastRefresh}
+      <table className="px-6 py-4 whitespace-nowrap text-sm text-yellow mt-5 border-2 rounded-xl bg-gray-100 text-black">
+        {
+          rows.map(
+            (row) => { return (<tr>
+              <td className="px-6 py-4 whitespace-nowrap text-sm ">
+                {row.timestamp}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm ">
+                {row.operatorId}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm ">
+                {row.activityName}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm ">
+                {row.gravity}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm ">
+                {row.raspberryName}
+              </td>
+              </tr>)}
+          )
+        }
+      </table>
+    </main>
+  );
+}
