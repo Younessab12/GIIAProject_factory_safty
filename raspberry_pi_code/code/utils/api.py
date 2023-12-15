84574b8808a0ab8718422269aecf693633251eee
AmineF0 <aminefirdawsi@gmail.com>
2023-12-15 17:34:50 +0100

    a

--- raspberry_pi_code/code/utils/api.py
+++ raspberry_pi_code/code/utils/api.py
@@ -1,9 +1,28 @@
+import requests
+import time
+import json
+
 class API:
+  def __init__(self, baseUrl, apiKey):
+    self.baseUrl = baseUrl
+    self.apiKey = apiKey
 
-  def upload_image(img, metadata):
-    pass
-
-  def upload_results_to_server(results):
-    # timestamp
-
-    pass
+  def reportActivity(self, activity):
+    """
+      apiKey: z.string(),
+      operatorName: z.string(),
+      gravity: z.number(),
+      activityName: z.string(),
+      timestamp: z.string(),
+    """
+    timestamp = int(time.time())
+    payload = {
+      'apiKey': self.apiKey,
+      'operatorName': activity['operatorName'],
+      'gravity': activity['gravity'],
+      'activityName': activity['activityName'],
+      'timestamp': timestamp
+    }
+    url = self.baseUrl + '/api/reportActivity'
+    return requests.post(url, json=payload)
+  
