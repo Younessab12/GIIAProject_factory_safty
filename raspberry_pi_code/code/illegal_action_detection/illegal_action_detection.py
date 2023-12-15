db1cbfbe49a70edd1269317061c75028190a6419
AmineF0 <aminefirdawsi@gmail.com>
2023-12-16 00:50:17 +0100

    last_version

--- raspberry_pi_code/code/illegal_action_detection/illegal_action_detection.py
+++ raspberry_pi_code/code/illegal_action_detection/illegal_action_detection.py
@@ -2,12 +2,18 @@ import illegal_action_detection.phoneusing as phoneusing
 import illegal_action_detection.smocking as smocking
 
 class IllegalActionDetection:
+
     def __init__(self):
         self.phoneusing = phoneusing.PhoneDetector()
         self.smocking = smocking.SmockingDetector()
 
-    def detect(self, frame, mp):
-        detection = {}
-        # detection['phoneusing'] = self.phoneusing.detect(frame)
-        detection['smocking'] = self.smocking.detect(frame)
+    def detect(self, frame, mp, objects_to_detect = ['phoneusing', 'smocking']):
+        detection = {
+            'phoneusing': None,
+            'smocking': None
+        }
+        if 'phoneusing' in objects_to_detect:
+            detection['phoneusing'] = self.phoneusing.detect(frame)
+        if 'smocking' in objects_to_detect:
+            detection['smocking'] = self.smocking.detect(frame)
         return detection
